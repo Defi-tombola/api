aa885df0623e937c9ff3c780eb62cfa01fc8190b
robertprp <robertforperez@gmail.com>
2025-03-02 12:50:46 +0100

    fix: pipeline

--- .github/scripts/build-and-push.sh
+++ .github/scripts/build-and-push.sh
@@ -9,11 +9,11 @@ REGISTRY="fra.vultrcr.com/tombola"
 
 # build and push images
 ECR_INDEXER_REPOSITORY="indexer"
-docker build --platform="linux/arm64" -t $ECR_INDEXER_REPOSITORY:latest -f docker/$ECR_INDEXER_REPOSITORY/Dockerfile .
+docker build --platform="linux/amd64" -t $ECR_INDEXER_REPOSITORY:latest -f docker/$ECR_INDEXER_REPOSITORY/Dockerfile .
 docker tag $ECR_INDEXER_REPOSITORY:latest $REGISTRY/$ECR_INDEXER_REPOSITORY:latest
 docker push $REGISTRY/$ECR_INDEXER_REPOSITORY:latest
 
 ECR_GRAPHQL_REPOSITORY="graphql"
-docker build --platform="linux/arm64" -t $ECR_GRAPHQL_REPOSITORY:latest -f docker/$ECR_GRAPHQL_REPOSITORY/Dockerfile .
+docker build --platform="linux/amd64" -t $ECR_GRAPHQL_REPOSITORY:latest -f docker/$ECR_GRAPHQL_REPOSITORY/Dockerfile .
 docker tag $ECR_GRAPHQL_REPOSITORY:latest $REGISTRY/$ECR_GRAPHQL_REPOSITORY:latest
 docker push $REGISTRY/$ECR_GRAPHQL_REPOSITORY:latest
